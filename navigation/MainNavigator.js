d57fdb9aa0cfae64e3ae8cfda84dfe16025d1b32
muticansel <cancel.muti@gmail.com>
2020-03-04 15:10:27 +0200

    startup screen

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,8 +1,10 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { AuthStackNavigator } from './StackNavigators';
 import { CfCankayaNav } from './DrawerNavigator';
+import StartupScreen from '../components/screens/StartupScreen';
 
 const SwitchNavigator = createSwitchNavigator({
+    StartupScreen: StartupScreen,
     Auth: AuthStackNavigator,
     CFCankaya: CfCankayaNav
 })
